44ad2b5193ab4a595704985998145729a50a1afb
sajeeb5523 <sajeebaljabed1@gmail.com>
2026-05-03 18:39:09 +0600

    feat: display activity history

--- scripts/completedButton.js
+++ scripts/completedButton.js
@@ -1,30 +1,38 @@
 const buttons = document.querySelectorAll('.completed_btn');
-const calculation = document.getElementById('calculation');
-const calculationSum = document.getElementById('calculation_sum');
 
 buttons.forEach((btn) => {
     btn.addEventListener('click', function () {
 
+        // alert
         alert('Board updated Successfully');
+
+        const card = btn.closest('.rounded-xl');
+        const taskTitle = card.querySelector('.title_history').innerText;
+
+        // timestamp
+        const time = new Date().toLocaleTimeString();
+
 
         // button disabled
         btn.disabled = true;
         btn.style.opacity = '0.3';
         btn.style.cursor = 'not-allowed';
         btn.textContent = 'Completed';
-        btn.style.color = 'black';
 
         // subtract 1 from task assigned
-        let newCalculation = calculation.innerText - 1;
-        if (newCalculation > 0) {
-            calculation.innerText = newCalculation;
-        }
+        const calculation = document.getElementById('calculation');
+        calculation.innerText = calculation.innerText - 1;
 
-        // sum 1 from 23 task
-        let newCalculationSum = (+calculationSum.innerText) + 1;
-        if (newCalculationSum > 0) {
-            calculationSum.innerText = newCalculationSum;
-        }
+        // add 1 to total completed count
+        const calculationSum = document.getElementById('calculation_sum');
+        calculationSum.innerText = (+calculationSum.innerText) + 1;
 
+        // add history entry to activity log
+        const historySection = document.getElementById('history_section');
+
+        const p = document.createElement('p');
+        p.classList.add('bg-[#F4F7FF]', 'p-3', 'rounded-lg', 'mb-3', 'text-sm');
+        p.innerText = `You have completed the task ${taskTitle} at ${time}`;
+        historySection.appendChild(p);
     });
 });
